40353f77f9e2f90a7441bb85432b95342d2c6ece
Nitin Singh <nitinsingh@Nitins-MacBook-Air.local>
2021-07-22 10:47:54 +0530

    Login UI completed

--- ChatterBox/LoginViewController.swift
+++ ChatterBox/LoginViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: IBOUTLETS
+    @IBOutlet weak var emailLabelOutlet: UILabel!
+    @IBOutlet weak var passwordLabelOutlet: UILabel!
+    @IBOutlet weak var repeatLabelOutlet: UILabel!
+    @IBOutlet weak var signUpLabelOutlet: UIButton!
+    
+    @IBOutlet weak var repeatPasswordLineView: UIView!
+    
+    @IBOutlet weak var LoginButtonOutlet: UIButton!
+    @IBOutlet weak var signUpButtonOutlet: UIButton!
+    @IBOutlet weak var forgotPasswordOutlet: UIButton!
+    @IBOutlet weak var resendEmailPasswordOutlet: UIButton!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextfield: UITextField!
+    
+    //MARK: VIEW LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    //MARK: IBACTION
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resendEmailButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func forgetPasswordButtonTapped(_ sender: UIButton) {
+    }
 }
 
